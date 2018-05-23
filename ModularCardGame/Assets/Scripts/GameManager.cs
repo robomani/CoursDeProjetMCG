5d3fe41d13712f7a61cb976f2e4867ffdb048cd9
robomani <robomani@hotmail.com>
2018-05-23 19:31:41 -0400

    Added altar perefab and remade game board to no longer be static but be created at game start with number of tiles selected in editor.

---
+++ ModularCardGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int m_TilesBetweenAltars = 5;
+    public int m_TilesAlongAltars = 3;
+    public float m_DivideBetweenTiles = 0.5f;
+    public GameObject m_TilePrefab;
+    public GameObject m_AltarPrefab;
+    public GameObject[] m_Tiles;
+    public GameObject[] m_Altars;
+
+
+	private void Start ()
+    {
+        if (m_TilePrefab && m_AltarPrefab)
+        {
+            GameObject[] tileTemp = new GameObject[m_TilesAlongAltars * m_TilesBetweenAltars];
+            Vector3 nextPosition = transform.position;
+            m_Altars[0] = Instantiate(m_AltarPrefab, nextPosition, transform.rotation, transform) as GameObject;
+            m_Altars[0].transform.localScale = new Vector3(2.5f * m_TilesAlongAltars, 1f,2f);
+            nextPosition.x = transform.position.x - m_TilesAlongAltars;
+            nextPosition.z = transform.position.z + 2f + m_DivideBetweenTiles;
+            for (int i = 0; i < m_TilesAlongAltars; i++)
+            {
+                for (int j = 0; j < m_TilesBetweenAltars; j++)
+                {
+                    tileTemp[i * m_TilesBetweenAltars + j] = Instantiate(m_TilePrefab, nextPosition, transform.rotation, transform) as GameObject;
+                    nextPosition.z += 2f + m_DivideBetweenTiles;
+                }
+                nextPosition.x += m_TilesAlongAltars;
+                nextPosition.z = transform.position.z + 2f + m_DivideBetweenTiles;
+            }
+            m_Tiles = tileTemp;
+            nextPosition.z += 2f * (m_TilesBetweenAltars + 1) + m_DivideBetweenTiles;
+            nextPosition.x = transform.position.x;
+            m_Altars[1] = Instantiate(m_AltarPrefab, nextPosition, transform.rotation, transform) as GameObject;
+            m_Altars[1].transform.localScale = new Vector3(2.5f * m_TilesAlongAltars, 1f, 2f);
+        }
+	}
+	
+
+	private void Update ()
+    {
+		
+	}
+}
