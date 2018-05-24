64f435f81117ed5f4166725dd8222a10f49126fb
robomani <robomani@hotmail.com>
2018-05-24 12:08:33 -0400

    Start PlayerController script

---
+++ ModularCardGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int m_HP = 20;
+
+    private int m_Mana = 1;
+    private int m_MaxMana = 1;
+
+
+    public void Cast(int i_ManaCost)
+    {
+        m_Mana -= i_ManaCost;
+    }
+
+    public void RaiseMaxMana()
+    {
+        if (m_Mana > 0)
+        {
+            m_Mana = 0;
+            m_MaxMana++;
+        }
+    }
+}
